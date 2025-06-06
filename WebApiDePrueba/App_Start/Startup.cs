214c33b75468f8ec059dce3c7bce7d86a021bc0a
Ivan <bartolocastro@gmail.com>
2025-06-06 08:46:33 -0600

    se sube el proyecto inicial

---
+++ WebApiDePrueba/App_Start/Startup.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using WebApiDePrueba.DAL;
+
+[assembly: OwinStartup(typeof(WebApiDePrueba.App_Start.Startup))]
+namespace WebApiDePrueba.App_Start
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureOAuth(app);
+            //Rest of code is here;
+        }
+
+        public void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromHours(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+        }
+    }
+
+    public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+        }
+
+        public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+
+            context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+            var response = new DbWrapper().GetUserByUserNameAndPass(context.UserName, context.Password);
+            if (response == null)
+            {
+                context.SetError("invalid_grant", "The user name or password is incorrect.");
+                return;
+            }
+
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            identity.AddClaim(new Claim("sub", context.UserName));
+            identity.AddClaim(new Claim("role", "user"));
+
+            context.Validated(identity);
+
+        }
+    }
+}
---
+++ WebApiDePrueba/DAL/DbWrapper.Usuario.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using WebApiDePrueba.DAL;
+using WebApiDePrueba.Models;
+
+namespace WebApiDePrueba.DAL
+{
+	public partial class DbWrapper
+	{
+		public List<ObjUsuario> GetAllUsuario(out OperationResult result)
+		{
+            result = new OperationResult()
+            { 
+                Success = true
+            };
+            IEnumerable<ObjUsuario> response = new List<ObjUsuario>();
+            try
+            {
+                response = GetObjects("GetAllUsuario", System.Data.CommandType.StoredProcedure,
+                   new Func<System.Data.IDataReader, ObjUsuario>(r =>
+                   {
+                       var usuario = FillEntity<ObjUsuario>(r);
+
+                       return usuario;
+                   }));
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.ErrorMessage = "El sistema no esta disponible.";//ex.Message;
+            }
+			return response.ToList();
+		}
+        public ObjUsuario GetUserByUserNameAndPass(string userNama, string pass)
+        {
+            var parametros = new List<SqlParameter>()
+            { 
+                new SqlParameter()
+                { 
+                    Value = userNama,
+                    ParameterName = "@UserName"
+                },
+                new SqlParameter()
+                {
+                    Value = pass,
+                    ParameterName = "@Pass"
+                }
+            };
+            var response = GetObject<ObjUsuario>("GetUserByUserNameAndPass", CommandType.StoredProcedure, parametros,
+                new Func<System.Data.IDataReader, ObjUsuario>(r =>
+                {
+                    var usuario = FillEntity<ObjUsuario>(r);
+
+                    return usuario;
+                }));
+
+            return response;
+        }
+    }
+}
