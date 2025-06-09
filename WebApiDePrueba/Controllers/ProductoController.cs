934aa91f6fe71855dc9a6965e75de2d547d853dc
DcRael <israelgarcia.ciatsoftware@gmail.com>
2025-06-09 15:16:05 -0600

    primera actualizacion

---
+++ WebApiDePrueba/Controllers/ProductoController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiDePrueba.DAL;
+using WebApiDePrueba.Models;
+
+namespace WebApiDePrueba.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/Producto")]
+    public class ProductoController : ApiController
+    {
+        private DbWrapper wrapper;
+        public ProductoController()
+        {
+            wrapper = new DbWrapper();
+            
+        }
+
+        [HttpGet]
+        [Route("GetAllProductos")]
+        public ModelResponse GetAllProductos()
+        {
+            var response = new ModelResponse()
+            {
+                Response = wrapper.GetAllProductos(out OperationResult result),
+                Result = result
+            };
+
+            return response;
+
+        }
+        [HttpPost]
+        [Route("GetProductosForId")]
+        public ModelResponse GetProductosForId(ObjProducto request)
+        {
+            var response = new ModelResponse()
+            {
+                Response = wrapper.GetProductosForId(request.Id, out OperationResult result),
+                Result = result
+            };
+
+            return response;
+        }
+    }
+}
---
+++ WebApiDePrueba/DAL/DbWrapper.Producto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Management;
+using WebApiDePrueba.Models;
+
+namespace WebApiDePrueba.DAL
+{
+    public partial class DbWrapper
+    {
+        public List<ObjProducto> GetAllProductos(out OperationResult result)
+        {
+            result = new  OperationResult()
+            {
+                Success= true
+            };
+            IEnumerable<ObjProducto> response = new List<ObjProducto>();
+
+            try
+            {
+                response = GetObjects<ObjProducto>("GetAllProductos", System.Data.CommandType.StoredProcedure,
+                    new Func<System.Data.IDataReader, ObjProducto>(r =>
+                    {
+                        var producto = FillEntity<ObjProducto>(r);
+                        return producto;
+                        }));
+            }
+            catch (Exception ex)
+            {
+                result.Success = false;
+                result.ErrorMessage = ex.Message;
+                throw;
+            }
+            return response.ToList();
+        }
+        public ObjProducto GetProductosForId(int Id, out OperationResult result)
+        {
+            result = new OperationResult()
+            {
+                Success = true
+            };
+
+            var parametros = new List<SqlParameter>()
+            {
+                new SqlParameter()
+                {
+                    Value = Id,
+                    ParameterName = "@Id"
+                }
+            };
+            ObjProducto response = new ObjProducto();
+            try
+            {
+                response = GetObject<ObjProducto>("GetProductosForId", CommandType.StoredProcedure, parametros,
+                    new Func<System.Data.IDataReader, ObjProducto>(r =>
+                    {
+                        var producto = FillEntity<ObjProducto>(r);
+
+                        return producto;
+                    }));
+            }
+            catch
+            (Exception ex)
+            {
+                result.Success = false;
+                result.ErrorMessage = ex.Message;
+                throw;
+            }
+
+            return response;
+        }
+    }
+}
