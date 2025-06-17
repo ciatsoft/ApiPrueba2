48aad7f34cd1805e62d55613d13f62d76afc3ee2
Ivan <bartolocastro@gmail.com>
2025-06-17 08:42:23 -0600

    Se agrega validador con jquery en la ventana de publicaciones

---
+++ MVCDePrueba/Scripts/Publicaciones/Crud.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    ValidadorDeDatos();
+});
+
+function ValidadorDeDatos() {
+    $("#frmPublicacionesCrud").validate({
+        rules: {
+            "Titulo": {
+                required: true
+            },
+            "Autor": {
+                required: true,
+                maxlength: 5
+            }
+        },
+        messages: {
+            Titulo: "El campo titulo es requerido",
+            "Autor": {
+                required: "El campo es requerido",
+                maxlength: "No puedes ingresar más de 5 caracteres"
+            }
+        }
+    });
+}
